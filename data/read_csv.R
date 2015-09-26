1255f47f28db591d2cb9c740d9d7a59ace41d7d7
KjartanMa <kjartanm2@gmail.com>
2015-09-26 16:03:53 +0000

    Data manipulation

--- data/read_csv.R
+++ data/read_csv.R
@@ -1,18 +1,55 @@
 original_dir <- getwd()
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-#Interesting
-leiguverd <- read.csv('csv/arid2014leiguverd_260220150.csv', header=TRUE, skip=3, dec=',', as.is=TRUE)
-# Taka saman mánuði:
-makaskipti_hbsv <- read.csv('csv/fjoldi_makaskipta_ibudir_hbsv_1993-2006_eftir_kaupdegi0.csv', header = TRUE, dec=',', as.is=TRUE)
-# Verð eftir ári.
+
+# Hjálparföll
+
+# xList er þau gögn sem á að aggregate-a.
+# byList er sá listi sem aggregate-a á eftir.
+custom_aggregate <- function(xList, byList, func)
+{
+  return(aggregate(as.numeric(lapply(xList, function (x) {replace(x, is.na(x) | x == '.', 0)}))
+                   ,by=byList, FUN=func, na.rm=TRUE, na.naction=NULL))
+}
+
+# dataentity er t.d. data.frame eða data.table
+# columnname er nafn þess dálks sem við viljum einangra úr dataentity
+extract_column <- function(dataentity, columnname)
+{
+  return(subset(dataentity, select=c(columnname)))
+}
+
+# Verð eftir ári (WELL FORMATED).
 hbsv_eftir_ar_all <- read.csv("csv/hbsv_eftir_ár_allt_saman_201502260.csv", header = TRUE, skip=1, nrows=34)
 hbsv_eftir_ar_fjol <- read.csv("csv/hbsv_eftir_ár_allt_saman_201502261.csv", header = TRUE, skip=1, nrows = 34)
 hbsv_eftir_ar_ein <- read.csv("csv/hbsv_eftir_ár_allt_saman_201502262.csv", header = TRUE, skip=1, nrows=34)
-# Upplýsingar um verð eftir landshlutum.
+# Fjöldi íbúða.
 landshlutar <- read.csv("csv/Landshlutar_201502250.csv", header=TRUE, skip=1)
-# Makaskipti
+fjoldi_husa_hbsv <- landshlutar[7:27,]
+names(fjoldi_husa_hbsv)[1] <- "Ár"
+# Samningar + Makaskipti
+makaskipti_hbsv <- read.csv('csv/fjoldi_makaskipta_ibudir_hbsv_1993-2006_eftir_kaupdegi0.csv', header = TRUE, dec=',', as.is=TRUE)
+makaskipti <- makaskipti_hbsv[c(1,3,4)]
+makaskipti <- subset(data.frame(custom_aggregate(makaskipti$Makaskipti, list(makaskipti$Ár), sum),
+                                extract_column(custom_aggregate(makaskipti$Samningar, list(makaskipti$Ár), sum), 'x')),
+                     Group.1 >= 1994)
+colnames(makaskipti) <- c('Ár', 'Makaskipti', 'Samningar')
 makaskipti_hbsv_1 <- read.csv("csv/Makaskipti_hbsv_201502250.csv", header=TRUE, skip=1)
+makaskipti_hbsv_1 <- makaskipti_hbsv_1[c(1, 3, 4, 5, 6)]
+makaskipti_2006 <- subset(data.frame(custom_aggregate(makaskipti_hbsv_1$Makaskiptasamningar, list(makaskipti_hbsv_1$Ár), sum),
+                                extract_column(custom_aggregate(makaskipti_hbsv_1$Sölur, list(makaskipti_hbsv_1$Ár), sum), 'x')),
+                                Group.1 >= 1994)
+colnames(makaskipti_2006) <- c('Ár', 'Makaskipti', 'Samningar')
+makaskipti <- rbind(makaskipti, makaskipti_2006)
+makaskipti$Ár <- as.integer(makaskipti$Ár)
+makaskipti <- subset(data.frame(custom_aggregate(makaskipti$Makaskipti, list(makaskipti$Ár), sum),
+                                extract_column(custom_aggregate(makaskipti$Samningar, list(makaskipti$Ár), sum), 'x')),
+                     Group.1 >= 1994)
+colnames(makaskipti) <- c('Ár', 'Makaskipti', 'Samningar')
+
+#Interesting (split into years in hbsv)
+leiguverd <- read.csv('csv/arid2014leiguverd_260220150.csv', header=TRUE, skip=3, dec=',', as.is=TRUE)
+# Makaskipti (SAMA og aður)
 makaskipti_utan <- read.csv("csv/Makaskipti_utan_hbsv_201502250.csv", header=TRUE, skip=1)
 # Fjoldi ibua allt land.
 fjoldi_ibuda <- read.csv("csv/Skipting_ibuda_eftir_svfn__201508240.csv", header=TRUE, skip=1)
