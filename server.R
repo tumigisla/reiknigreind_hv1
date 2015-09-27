05e74794b32ecc7b37a20bd4d523150f9f7e5a65
KjartanMa <kjartanm2@gmail.com>
2015-09-27 17:31:17 +0000

    More changes

--- server.R
+++ server.R
@@ -3,8 +3,9 @@ library(shiny)
 shinyServer(function(input, output) {
   yaxisVariable <- reactive({input$yaxis})
   xaxisVariable <- reactive({input$xaxis})
+  sizeVariable <- reactive({input$sizeaxis})
   
   output$view <- renderGvis({
-    gvisBubbleChart(masterFrame_wo_NAs, idvar="Ár", xvar=xaxisVariable(), yvar=yaxisVariable())
+    gvisBubbleChart(masterFrame_wo_NAs, idvar="Ár", xvar=xaxisVariable(), yvar=yaxisVariable(), colorvar = sizeVariable(), sizevar = sizeVariable())
   })
 })
