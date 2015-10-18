4db941e7417204f0cb6c5defe86f31a9743f8510
KjartanMa <kjartanm2@gmail.com>
2015-10-18 15:23:41 +0000

    Breyta UI uppbyggingu

--- server.R
+++ server.R
@@ -6,7 +6,8 @@ shinyServer(function(input, output) {
   sizeVariable <- reactive({input$sizeaxis})
   
   output$view <- renderGvis({
-    options = list(width=1000, height=1000, hAxis=paste0('{title: "', xaxisVariable(),'"}'), vAxis = paste0('{title: "', yaxisVariable(), '"}'))
+    options = list(width=1000, height=1000, hAxis=paste0('{title: "', xaxisVariable(),'"}'), vAxis = paste0('{title: "', yaxisVariable(), '"}'),
+                   colorAxis = '{legend: {position: "none"} }')
     gvisBubbleChart(masterFrame, idvar="Ár", xvar=xaxisVariable(), yvar=yaxisVariable(), colorvar = "Ár", sizevar = sizeVariable(), options=options)
   })
 })
